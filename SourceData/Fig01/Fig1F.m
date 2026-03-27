a92538fd1003d8636f60d7c14038c62176c111fd
XuChun_Lab <87345360+XuChunLab@users.noreply.github.com>
2026-03-27 12:11:52 +0800

    Add files via upload

---
+++ SourceData/Fig01/Fig1F.m
@@ -0,0 +1,90 @@
+
+load('Fig1F.mat')
+
+addpath(genpath('Violinplot-Matlab-master'))
+
+
+figure('Position',[100 100 100 300])
+grouporder={'DBv','DBx'}%,'J3v','J3x'}; 
+Origin={
+    'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';... 
+    'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';  'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';  'DBx';'DBx';'DBx';'DBx';'DBx';...
+    %'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';...
+    %'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';  'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';  'J3x';'J3x';'J3x';'J3x';'J3x';...
+    }; 
+MPG=[
+    DBandJ3v(:,1);
+    DBandJ3x(:,1);
+    %DBandJ3v(:,2);
+    %DBandJ3x(:,2);
+];
+h = violinplot(MPG,Origin,'GroupOrder',grouporder,'Width',0.3);
+box off
+xlim([0.4 2.6])
+ylim([0 15])
+
+
+figure('Position',[100 100 100 300])
+grouporder={'J3v','J3x'}%,'J3v','J3x'}; 
+Origin={
+    %'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';... 
+    %'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';  'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';  'DBx';'DBx';'DBx';'DBx';'DBx';...
+    'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';...
+    'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';  'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';  'J3x';'J3x';'J3x';'J3x';'J3x';...
+    }; 
+MPG=[
+    %DBandJ3v(:,1);
+    %DBandJ3x(:,1);
+    DBandJ3v(:,2);
+    DBandJ3x(:,2);
+];
+h = violinplot(MPG,Origin,'GroupOrder',grouporder,'Width',0.3);
+box off
+xlim([0.4 2.6])
+
+ylim([0 15])
+%ylim([15 50])
+
+
+
+
+figure('Position',[100 100 100 300])
+grouporder={'DBv','DBx'}%,'J3v','J3x'}; 
+Origin={
+    'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';... 
+    'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';  'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';  'DBx';...
+    %'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';...
+    %'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';  'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';  'J3x';'J3x';'J3x';'J3x';'J3x';...
+    }; 
+MPG=[
+    DBandJ3v(:,1);
+    DBandJ3x(:,1);
+    %DBandJ3v(:,2);
+    %DBandJ3x(:,2);
+];
+h = violinplot(MPG,Origin,'GroupOrder',grouporder,'Width',0.3);
+box off
+xlim([0.4 2.6])
+ylim([0 30])
+
+
+figure('Position',[100 100 100 300])
+grouporder={'J3v','J3x'}%,'J3v','J3x'}; 
+Origin={
+    %'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';'DBv';  'DBv';'DBv';'DBv';... 
+    %'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';  'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';'DBx';  'DBx';'DBx';'DBx';'DBx';'DBx';...
+    'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';'J3v';  'J3v';'J3v';...
+    'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';  'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';'J3x';  'J3x';...
+    }; 
+MPG=[
+    %DBandJ3v(:,1);
+    %DBandJ3x(:,1);
+    DBandJ3v(:,2);
+    DBandJ3x(:,2);
+];
+h = violinplot(MPG,Origin,'GroupOrder',grouporder,'Width',0.3);
+box off
+xlim([0.4 2.6])
+
+ylim([0 8])
+%ylim([8 25])
